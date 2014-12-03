e873a8bd9e6912c6894a4f9178ac7fe5f8ef16d2
ctgschollar <ctgschollar@gmail.com>
2014-12-03 19:52:28 +0200

    SR-66 Changed search to allow users to see the actual reduction files as well as simply download a zipped version of all reductions. Added ability to search by date, observer and experiement ID. Altered search so that back button in browsers will now work as expected (mostly, I still need to work on the advanced search). Also made some cosmetic changes.

--- WebContent/js/solr-ajax/widgets/ResultWidget.js
+++ WebContent/js/solr-ajax/widgets/ResultWidget.js
@@ -1,124 +1,241 @@
-(function ($) {
+(function($) {
 
-AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
-  start: 0,
+	AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget
+			.extend({
+				start : 0,
 
-  beforeRequest: function () {
-    $(this.target).html($('<img>').attr('src', 'images/ajax-loader.gif'));
-  },
+				beforeRequest : function() {
+					$(this.target).html(
+							$('<img>').attr('src', 'images/ajax-loader.gif'));
+				},
 
-  facetLinks: function (facet_field, facet_values) {
-    var links = [];
-    if (facet_values) {
-      for (var i = 0, l = facet_values.length; i < l; i++) {
-        if (facet_values[i] !== undefined) {
-          links.push(
-            $('<a href="#"></a>')
-            .text(facet_values[i])
-            .click(this.facetHandler(facet_field, facet_values[i]))
-          );
-        }
-        else {
-          links.push('no items found in current selection');
-        }
-      }
-    }
-    return links;
-  },
+				facetLinks : function(facet_field, facet_values) {
+					var links = [];
+					if (facet_values) {
+						for (var i = 0, l = facet_values.length; i < l; i++) {
+							if (facet_values[i] !== undefined) {
+								links.push($('<a href="#"></a>').text(
+										facet_values[i]).click(
+										this.facetHandler(facet_field,
+												facet_values[i])));
+							} else {
+								links
+										.push('no items found in current selection');
+							}
+						}
+					}
+					return links;
+				},
 
-  facetHandler: function (facet_field, facet_value) {
-    var self = this;
-    return function () {
-      self.manager.store.remove('fq');
-      self.manager.store.addByValue('fq', facet_field + ':' + AjaxSolr.Parameter.escapeValue(facet_value));
-      self.doRequest();
-      return false;
-    };
-  },
+				facetHandler : function(facet_field, facet_value) {
+					var self = this;
+					return function() {
+						self.manager.store.remove('fq');
+						self.manager.store.addByValue('fq', facet_field + ':'
+								+ AjaxSolr.Parameter.escapeValue(facet_value));
+						self.doRequest();
+						return false;
+					};
+				},
 
-  //This function is called after every request of the manager (the response is accessible via this.manager.response)
-  afterRequest: function () {
-    $(this.target).empty(); //empty the target
-    for (var i = 0, l = this.manager.response.response.docs.length; i < l; i++) {
-      var doc = this.manager.response.response.docs[i];
-      $(this.target).append(this.template(doc));
+				// This function is called after every request of the manager
+				// (the response is accessible via this.manager.response)
+				afterRequest : function() {
+					$(this.target).empty(); // empty the target
+					for (var i = 0, l = this.manager.response.response.docs.length; i < l; i++) {
+						var doc = this.manager.response.response.docs[i];
+						$(this.target).append(this.template(doc));
 
-      var items = [];
-      items = items.concat(this.facetLinks(doc.Observer));
-      items = items.concat(this.facetLinks(doc.Description));
-      items = items.concat(this.facetLinks(doc.NumFreqChannels));
+						var items = [];
+						items = items.concat(this.facetLinks(doc.Observer));
+						items = items.concat(this.facetLinks(doc.Description));
+						items = items.concat(this
+								.facetLinks(doc.NumFreqChannels));
 
-      var $links = $('#links_' + doc.ProductId);
-      $links.empty();
-      for (var j = 0, m = items.length; j < m; j++) {
-        $links.append($('<li></li>').append(items[j]));
-      }
-    }
-  },
+						var $links = $('#links_' + doc.ProductId);
+						$links.empty();
+						for (var j = 0, m = items.length; j < m; j++) {
+							$links.append($('<li></li>').append(items[j]));
+						}
+					}
+				},
 
-  template: function (doc) {
-	  
-//	  var options;
-//	  var response;
-//	  var query = "ExperimentID:20140718-0001";
-//	  opts = {dataType: 'json'};
-//	  if (this.proxyUrl) {
-//	      options.url = this.proxyUrl;
-//	      options.data = {query: string};
-//	      options.type = 'POST';
-//	    }
-//	    else {
-//	      options.url = this.solrUrl + servlet + '?' + string + '&wt=json&json.wrf=?';
-//	    }
-//	  errorler = errorHandler || function (jqXHR, textStatus, errorThrown) {
-//	      Manager.handleError(textStatus + ', ' + errorThrown);
-//	    };
-//	   ler = function (data) {
-//		    response = data;};
-//	   jQuery.ajax(opts).done(ler).fail(errorler);
-	  var output = '<div class="result"><h2>' + doc.Description + '</h2>';
-	  
-	  var result = "No Change";
-	  var stat = "NOTHIN";
-	  var paused = true;
-	  $.post('http://kat-archive.kat.ac.za:8983/solr/collection1/select?q=ExperimentID:'+ doc['ExperimentID'] +' AND CAS.ProductTypeName:RTSReductionProduct&wt=json&json.wrf=?', {}, function(data,status){
-		    result = data;
-		     stat = status;
-//		     alert(JSON.stringify(result));
-		     if (result["response"]["docs"].length > 0)
-		    	 $("#files"+doc['ExperimentID']).append("<b><u> Reductions </u></b><br> ");
-		     
-		     for  (var i = 0; i < result["response"]["docs"].length; i++){
-		     link = "http://kat-archive.kat.ac.za:8983/fmprod/data?productID=" + result["response"]["docs"][i]["id"] + "&format=application/x-zip";
-		     $("#files"+doc['ExperimentID']).append("<b> "+ result["response"]["docs"][i]["ReductionName"] +"</b> : <a href = " + link + "> Download Zip </a> <br>");
-		     }}, "json");
-	  
-	  
-	var link = "http://kat-archive.kat.ac.za:8983/fmprod/data?productID=" + doc.id;
-	var filesize = parseFloat(doc.FileSize) / 1073741824;
-    var snippet = '';
-    var date = moment(doc['StartTime']);
-    snippet += "<b>Observer</b> : " + doc['Observer'] + "<br>";
-    snippet += "<b>Experiment ID</b> : " + doc['ExperimentID'] + "<br>";
-    snippet += "<b>Start Time</b> : " + date.format('YYYY-MM-DD HH:mm:ss') + "<br>";
-    snippet += "<div id= 'files"+doc['ExperimentID']+"'> <b> Observation Data </b> : <a href = " + link + " >" + doc.Filename + "</a> (" + filesize.toFixed(2) + " GB) <br></div>";
-    if (doc.Details != undefined) {
-		snippet +=  "<pre>" +
-		"<div class='collapsible'>" +
-		"<h3>Details</h3>" +
-		"<div class='details'><p>" + doc.Details + "</p></div>" +
-		"</div>" +
-		"</pre>";
-    }
-     output += '<p id="links_' + doc.ProductId + '" class="links"></p>';
-//     output += doc["CAS.ProductTypeName"];
-     output += '<p>' + snippet + '</p><hr></div>';
-     
-//     alert("POSTING");
-	  
-	  
-    return output;
-  }
-});
+				template : function(doc) {
+
+					var output = '<div class="result"><h2>' + doc.Description
+							+ '</h2>';
+
+					var result = "No Change";
+					var stat = "NOTHIN";
+					$
+							.post(
+									'http://kat-archive.kat.ac.za:8983/solr/collection1/select?q=ExperimentID:'
+											+ doc['ExperimentID']
+											+ ' AND CAS.ProductTypeName:RTSReductionProduct&wt=json&json.wrf=?',
+									{},
+									function(data, status) {
+										append = "";
+										result = data;
+										stat = status;
+										if (result["response"]["docs"].length > 0)
+											append += "<button type='button' class='seeReductions' id='btnSeeReductions"
+												+ doc['ExperimentID']
+											+"'> Show Reductions </button>"
+											+ "<div class='collapsible' id='collapseSeeReductions"
+											+ doc['ExperimentID']
+											+ "'> <br>"
+											+ "<div class='details'>";
+//											$("#files" + doc['ExperimentID'])
+//													.append("<button type='button' class='seeReductions' id='btnSeeReductions"
+//															+ doc['ExperimentID']
+//															+"> Show Reductions </button>"
+//															+ "<div class='collapsible' id='collapseSeeReductions"
+//															+ doc['ExperimentID']
+//															+ "'> <br>"
+//															+ "<div class='details'><p>");
+										for (var i = 0; i < result["response"]["docs"].length; i++){
+											
+										}
+
+										for (var i = 0; i < result["response"]["docs"].length; i++) {
+											link = "http://kat-archive.kat.ac.za:8983/fmprod/data?productID="
+													+ result["response"]["docs"][i]["id"]
+													+ "&format=application/x-zip";
+											append += "<table width = 100% ><tr><td>"
+												+ "<b> "
+												+ result["response"]["docs"][i]["ReductionName"]
+												+ "</b> : </td><td><a href = "
+												+ link
+												+ "><button type='button' style='float:right;'> Download Zip </button></a>";
+//											$("#files" + doc['ExperimentID'])
+//													.append(
+//															""
+//																	+ "<b> "
+//																	+ result["response"]["docs"][i]["ReductionName"]
+//																	+ "</b> : <a href = "
+//																	+ link
+//																	+ "> Download Zip </a>");
+											files = result["response"]["docs"][i]["CAS.ReferenceDatastore"];
+											fileshtml = "";
+
+											for (var j = 0; j < files.length; j++) {
+												url_deconstruct = files[j]
+														.split("/");
+												link = "http://kat-archive.kat.ac.za/archive/data/"
+														+ files[j]
+																.substring(
+																		27,
+																		files[j].length);
+												fileshtml += "<a href= "
+														+ link
+														+ ">"
+														+ url_deconstruct[url_deconstruct.length - 1]
+														+ "</a><br>";
+											}
+											append +="<button type='button' style='float:right;' class='seeFiles' id='btnSeeFiles"
+												+ doc['ExperimentID']
+											+ i
+											+ "'>Show file list</button></td></tr></table>"
+											+ "<div class='collapsible' id='collapseSeeFiles"
+											+ doc['ExperimentID']
+											+ i
+											+ "'> <br>"
+											+ "<div class='details'>"
+											+ fileshtml
+											+ "</div></div>";
+
+											
+											
+										}
+										$("#files" + doc['ExperimentID']).append(append);
+										console.log(append + "</div></div>");
+										
+										$("button").button();
+										$(".seeReductions")
+										.click(
+												function(event) {
+													collapsibleid = "#collapse"
+															+ this.id
+																	.substring(
+																			3,
+																			this.id.length);
+													console
+															.log(collapsibleid);
+													if ($(collapsibleid)
+															.hasClass(
+																	"opened")) {
+														$(collapsibleid)
+																.accordion(
+																		"option",
+																		"active",
+																		false);
+													} else {
+														$(collapsibleid)
+																.accordion(
+																		"option",
+																		"active",
+																		0);
+													}
+
+												});
+										$(".seeFiles")
+												.click(
+														function(event) {
+															collapsibleid = "#collapse"
+																	+ this.id
+																			.substring(
+																					3,
+																					this.id.length);
+															console
+																	.log(collapsibleid);
+															if ($(collapsibleid)
+																	.hasClass(
+																			"opened")) {
+																$(collapsibleid)
+																		.accordion(
+																				"option",
+																				"active",
+																				false);
+															} else {
+																$(collapsibleid)
+																		.accordion(
+																				"option",
+																				"active",
+																				0);
+															}
+
+														});
+									}, "json");
+
+					var link = "http://kat-archive.kat.ac.za:8983/fmprod/data?productID="
+							+ doc.id;
+					var filesize = parseFloat(doc.FileSize) / 1073741824;
+					var snippet = '';
+					var date = moment(doc['StartTime']);
+					snippet += "<b>Observer</b> : " + doc['Observer'] + "<br>";
+					snippet += "<b>Experiment ID</b> : " + doc['ExperimentID']
+							+ "<br>";
+					snippet += "<b>Start Time</b> : "
+							+ date.format('YYYY-MM-DD HH:mm:ss') + "<br>";
+					snippet += "<div id= 'files" + doc['ExperimentID']
+							+ "'> <b> Observation Data </b> : <a href = "
+							+ link + " >" + doc.Filename + "</a> ("
+							+ filesize.toFixed(2) + " GB) <br></div>";
+					if (doc.Details != undefined) {
+						snippet += "<pre>" + "<div class='collapsible'>"
+								+ "<h3>Details</h3>"
+								+ "<div class='details'><p>" + doc.Details
+								+ "</p></div>" + "</div>" + "</pre>";
+					}
+					output += '<p id="links_' + doc.ProductId
+							+ '" class="links"></p>';
+					// output += doc["CAS.ProductTypeName"];
+					output += '<p>' + snippet + '</p><hr></div>';
+
+					// alert("POSTING");
+
+					return output;
+				}
+			});
 
 })(jQuery);
--- WebContent/js/solr-ajax/widgets/btns.js
+++ WebContent/js/solr-ajax/widgets/btns.js
@@ -4,10 +4,15 @@ $(function() {
 	
 	$( "#btnSearch" ).click(function( event ) {
 		console.log($( '#inpSearch' ).val());
-		Manager.store.addByValue('q','(CAS.ProductTypeName:KatFile OR CAS.ProductTypeName:RTSTelescopeProduct) AND ' + $( '#inpSearch' ).val());
-		Manager.store.addByValue('sort', 'StartTime desc');
-		Manager.doRequest();
+		search();
+		
 	});
+	
+	$( "#inpSearch" ).keyup(function(e){
+	    if(e.keyCode == 13)
+	    {
+	    	search();
+	    }});
 	
 	$( "#btnSearchOptions").click(function (event){
 		if ( $( "#searchOptions").hasClass("opened") ){
@@ -15,6 +20,17 @@ $(function() {
 		}
 		else{
 			$( "#searchOptions").accordion( "option","active", 0 );
+		}
+	});
+	
+	$( ".seeFiles" ).click(function (event){
+		collapsibleid = "#collapseSeeFiles" + this.id.substring(3,this.id.length);
+		console.log(collapsibleid);
+		if ( $( collapsibleid ).hasClass("opened") ){
+			$( collapsibleid ).accordion( "option","active", false );
+		}
+		else{
+			$( collapsibleid ).accordion( "option","active", 0 );
 		}
 	});
 	
@@ -26,14 +42,78 @@ $(function() {
 	$( "#dpTo" ).datepicker({
 		maxDate:0,
 		onClose: function(selectedDate){
-		$( '#dpFrom' ).datepicker("option","minDate", selectedDate);
+		$( '#dpFrom' ).datepicker("option","maxDate", selectedDate);
 	}});
 });
+
+function search(){
+	Manager.store.remove('fq');
+	searchval = $( '#inpSearch' ).val();
+	if (searchval == "")
+		searchval = "*";
+	Manager.store.addByValue('q','(CAS.ProductTypeName:KatFile OR CAS.ProductTypeName:RTSTelescopeProduct) AND ' + searchval);
+	Manager.store.addByValue('sort', 'StartTime desc');
+	startDate = $( "#dpFrom" ).datepicker("getDate");
+	endDate = $( "#dpTo" ).datepicker("getDate");
+	observer = $( '#inpObserver' ).val();
+	experimentID = $( '#inpExpID' ).val();
+	if (startDate != null & endDate != null){
+		//This line grabs the times from the dateselectors, subtracts 2 hours to get to SAST and adds the range to the query
+		Manager.store.addByValue('fq',"StartTime:[" + moment(startDate).subtract(2,'hours').format('YYYY-MM-DDTHH:mm:ss[Z]') + " TO " + moment(endDate).subtract(2,'hours').format('YYYY-MM-DDTHH:mm:ss[Z]') + "]");
+	}
+	else if (startDate != null){
+		Manager.store.addByValue('fq',"StartTime:[" + moment(startDate).subtract(2,'hours').format('YYYY-MM-DDTHH:mm:ss[Z]') + " TO NOW]");
+	}
+	console.log (observer);
+	if (observer != "")
+		Manager.store.addByValue('fq',"Observer:"+observer);
+	if (experimentID != ""){
+		Manager.store.addByValue('fq',"ExperimentID:"+experimentID);
+	}
+	Manager.doRequest();
+	history.pushState({"search":$( '#inpSearch' ).val(),
+		'startDate': startDate,
+		'endDate': endDate}, '', $(this).attr("href"));
+}
+
+//Handle pressing back button
+$(window).on("popstate", function() {
+	state = history.state;
+	console.log(state);
+	if (state == null)
+		state = {"search":"*:*"};
+	Manager.store.addByValue('q','(CAS.ProductTypeName:KatFile OR CAS.ProductTypeName:RTSTelescopeProduct) AND ' + state["search"]);
+	Manager.store.addByValue('sort', 'StartTime desc');
+	$( '#inpSearch' ).val( state["search"] );
+	if (startDate != null & endDate != null){
+		//This line grabs the times from the dateselectors, subtracts 2 hours to get to SAST and adds the range to the query
+		Manager.store.addByValue('fq',"StartTime:[" + moment(startDate).subtract(2,'hours').format('YYYY-MM-DDTHH:mm:ss[Z]') + " TO " + moment(endDate).subtract(2,'hours').format('YYYY-MM-DDTHH:mm:ss[Z]') + "]");
+	}
+	Manager.doRequest();
+//    loadPage(location.href);
+  });
+
+//function clickHandler(e) {
+////  history.pushState({"search":$( '#inpSearch' ).val()}, null, "http://kat-archive.kat.ac.za:8983/Archive_Browser/");
+//  history.pushState(data, event.target.textContent, event.target.href);
+//  console.log("Woo!");
+//
+//  return event.preventDefault();
+//}
+
+//$(function() {
+//	  $("button").click(function() {
+//	    history.pushState({"Search:"}, '', $(this).attr("href"));
+//	    return false;
+//	  });
+//	});
+
 
 $( document ).ajaxComplete(function() {
 $( ".collapsible" ).accordion({
 	collapsible: true,
 	active: false,
+	heightStyle:"content",
 	activate: function(event, ui) {
 		if ( $( this ).hasClass("opened") ){
 			$( this ).removeClass("opened");
